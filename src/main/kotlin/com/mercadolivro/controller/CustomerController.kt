ed4856df66e62fb6c170869101ea73d756177603
le0cadio <guilhermeleocadio1301@gmail.com>
2025-08-13 21:37:16 -0300

    wip - customer controller and customer model

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,12 +1,16 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
+@RequestMapping("customers")
 class CustomerController {
+
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello World"
+    fun getCustomer(): String {
+        return "Customer 1"
     }
 }
---
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,4 @@
+package com.mercadolivro.model
+
+class CustomerModel {
+}
