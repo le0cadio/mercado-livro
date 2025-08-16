0569fa637daa870ce638fd1e572ad73a45da2cee
le0cadio <guilhermeleocadio1301@gmail.com>
2025-08-16 17:44:36 -0300

    creating a put method

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,11 +1,13 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -36,5 +38,14 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id: String): CustomerModel {
         return customers.first { it.id == id }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun putCustomer(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customers.first { it.id == id }.let{
+            it.name = customer.name
+            it.email = customer.email
+        }
     }
 }
---
+++ src/main/kotlin/com/mercadolivro/controller/request/PutCustomerRequest.kt
@@ -0,0 +1,5 @@
+package com.mercadolivro.controller.request
+
+data class PutCustomerRequest(
+    var name: String, var email: String
+)
