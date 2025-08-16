ebd3d03cd7e8a42b054db69a600899cffff52dc4
le0cadio <guilhermeleocadio1301@gmail.com>
2025-08-16 17:36:34 -0300

    set get to recieve customer by id

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,16 +1,15 @@
 package com.mercadolivro.controller
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 
 @RestController
@@ -20,7 +19,7 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): List<CustomerModel> {
+    fun getAllCustomers(): List<CustomerModel> {
         return customers
     }
 
@@ -32,5 +31,10 @@ class CustomerController {
             else -> customers.last().id.toInt() + 1
         }.toString()
         customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.first { it.id == id }
     }
 }
